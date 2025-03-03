7299b32987819c2df5dddbb474d3d58b581f6b5b
Klemen Kubelj <klemen.kubelj@gmail.com>
2025-03-03 19:22:04 +0100

    3RP for SI poplave

--- src/cvar/hazard_models/si_poplave.py
+++ src/cvar/hazard_models/si_poplave.py
@@ -25,28 +25,17 @@ logger = logging.getLogger(__name__)
 @dataclass
 class BatchItem:
     path: str
-    scenario: str
-    year: str
     filename_return_period: str  # the filename of the input
 
 
 class SIPoplaveSource:
     def __init__(self, source_dir):
         self.source_dir = source_dir
-
-    # def load_file(self, rp: int) -> gpd.GeoDataFrame:
-    #     path = os.path.join(self.source_dir, f"globine_q{rp}.gpkg")
-    #     print("Loading file: ", path)
-    #     return gpd.read_file(path)
-
-    # def razterize_flood_depth(self, df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    #     pass
     
     @contextmanager
     def load_file(self, fname: str):
         da: Optional[xr.DataArray] = None
         fpath = os.path.join(self.source_dir, fname)
-        print("Loading file: ", fpath)
         f = None
         try:
             f = open(fpath, "rb")
@@ -58,113 +47,6 @@ class SIPoplaveSource:
             if f is not None:
                 f.close()
 
-    # def open_
-
-    # def prepare(self, working_dir: Optional[str] = None):
-    #     path = ""
-    #     self.df = gpd.read_file(path)
-
-    # def open_dataset_year(
-    #     self, gcm: str, scenario: str, quantity: str, year: int, chunks=None
-    # ) -> xr.Dataset:
-    #     """_summary_
-
-    #     Args:
-    #         gcm (str): Ignored.
-    #         scenario (str): Ignored.
-    #         quantity (str): 'RiverineInundation' or 'CoastalInundation'.
-    #         year (int): Ignored.
-    #         chunks (_type_, optional): _description_. Defaults to None.
-
-    #     Returns:
-    #         xr.Dataset: Data set named 'indicator' with 'max' and 'min' coordinate labels in the index coordinate.
-    #     """
-    #     hazard_type = quantity
-
-    #     def get_merged_rp(row, min_max: str, flood_type: str):
-    #         """Calculate min or max from database entry (GeoDataFrame row).
-
-    #         From the paper: "In practice, if information is available in the design layer for a given sub-country unit, then
-    #         this information is included in the merged layer. If no information is contained in the design layer, then the policy layer
-    #         information is included in the merged layer. Finally, if information is not available even at the policy layer, then the
-    #         model layer information is included in the merged layer."
-
-    #         Args:
-    #             row: GeoDataFrame row
-    #             flood_type (str, optional): "Riv" or "Co". Defaults to "Riv".
-
-    #         Returns:
-    #             float: Protection level as return period in years.
-    #         """
-    #         layers = ["DL", "PL", "ModL"] if flood_type == "Riv" else ["DL", "PL"]
-    #         for layer in layers:  # design layer, policy layer, modelled layer
-    #             # note that for the modelled layer, both min and max are set to the modelled value
-    #             layer_rp = (
-    #                 row[f"{layer}_{flood_type}"]
-    #                 if layer == "ModL"
-    #                 else row[f"{layer}_{min_max}_{flood_type}"]
-    #             )
-    #             if layer_rp > 0:  # if 0, layer is considered missing
-    #                 # if design layer is present, use this, otherwise use the policy layer, otherwise the modelled, otherwise missing
-    #                 return layer_rp
-    #         return float("Nan")  # zero is no data, represented by NaN here.
-
-    #     logger.info(f"Processing hazard type {hazard_type}")
-    #     min_shapes: List[Tuple[float, Any]] = []
-    #     max_shapes: List[Tuple[float, Any]] = []
-    #     logger.info("Inferring max and min protection levels per region")
-    #     for _, row in self.df.iterrows():
-    #         flood_type = (
-    #             "Riv" if hazard_type == "RiverineInundation" else "Co"
-    #         )  # riverine and coastal
-    #         min, max = (
-    #             get_merged_rp(row, "Min", flood_type),
-    #             get_merged_rp(row, "Max", flood_type),
-    #         )
-    #         if row["name"] is None and (min is None and max is None):
-    #             continue
-    #         # if either the min or max is NaN, that is OK: the vulnerability model is expected to deal with that
-    #         if not math.isnan(min) and not math.isnan(max) and min > max:
-    #             # it can occur that for a layer there is only information about minimum
-    #             raise ValueError("unexpected return period")
-
-    #         if not math.isnan(min):
-    #             min_shapes.append((row.geometry, min))
-    #         if not math.isnan(max):
-    #             max_shapes.append((row.geometry, max))
-
-    #     resolution_in_arc_mins = 1
-    #     width, height = (
-    #         int(60 * 360 / resolution_in_arc_mins),
-    #         int(60 * 180 / resolution_in_arc_mins),
-    #     )
-    #     crs, transform = global_crs_transform(width, height)
-    #     logger.info("Creating empty array")
-    #     da = empty_data_array(
-    #         width,
-    #         height,
-    #         transform,
-    #         str(crs),
-    #         index_name="min_max",
-    #         index_values=["min", "max"],
-    #     )
-    #     for min_max in ["min", "max"]:
-    #         logger.info(
-    #             f"Creating raster at {(360 * 60) / width} arcmin resolution for {min_max} protection"
-    #         )
-    #         rasterized = features.rasterize(
-    #             min_shapes if min_max == "min" else max_shapes,
-    #             out_shape=[height, width],
-    #             transform=transform,
-    #             all_touched=True,
-    #             fill=float("nan"),  # background value
-    #             merge_alg=MergeAlg.replace,
-    #         )
-    #         index = 0 if min_max == "min" else 1
-    #         da[index, :, :] = rasterized[:, :]
-    #     return da.to_dataset(name="sop")
-
-
 
 class SIFloodIndicatorModel(IndicatorModel):
     """On-board the SI Flood model data set from 
@@ -172,23 +54,17 @@ class SIFloodIndicatorModel(IndicatorModel):
     """
 
     def __init__(self):
-        pass
         self.return_periods = [
-            10, 
-            # 100, 
-            # 500
+            10,
+            100,
+            500,
         ]
-
-    # def _resource(self, path):
-    #     return self.resources[path]
 
     def batch_items(self) -> Iterable[BatchItem]:
         items = [
             BatchItem(
-                path="inundation/si_poplave/v1/si_poplave", # Structur of output path
-                scenario="historical", # ?
-                year=1971, # ?
-                filename_return_period="globine_q{return_period}.tif", # Input filename
+                path="inundation/si_poplave/v1/si_poplave_historical_2025", # Structure of output path
+                filename_return_period="globine_q{return_period}_10.tif", # Input filename
             ),
         ]
         return items
@@ -199,8 +75,12 @@ class SIFloodIndicatorModel(IndicatorModel):
         print("SOURCE: ", source)
         print("TARGET: ", target)
         print("type: ", type(source))
-        assert isinstance(source, SIPoplaveSource), "Source is type: " + type(source)
-        assert isinstance(target, OscZarr), "Target is type: " + type(target)
+        assert isinstance(source, SIPoplaveSource), "Source is type: " + str(type(source))
+        assert isinstance(target, OscZarr), "Target is type: " + str(type(target))
+        
+        epsg_3912 = """
+        PROJCS["MGI_1901_Slovene_National_Grid",GEOGCS["GCS_MGI_1901",DATUM["D_MGI_1901",SPHEROID["Bessel_1841",6377397.155,299.1528128]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["False_Easting",500000.0],PARAMETER["False_Northing",-5000000.0],PARAMETER["Central_Meridian",15.0],PARAMETER["Scale_Factor",0.9999],PARAMETER["Latitude_Of_Origin",0.0],UNIT["Meter",1.0]]
+        """.replace("\n", "")
 
         for i, ret in enumerate(self.return_periods):
             print(f"Copying return period {i + 1}/{len(self.return_periods)}")
@@ -222,7 +102,7 @@ class SIFloodIndicatorModel(IndicatorModel):
                             da.transform[4],
                             da.transform[5],
                         ),
-                        str(CRS.from_epsg(3912)),
+                        epsg_3912,
                         index_values=self.return_periods,
                     )
                 # ('band', 'y', 'x')
---
+++ src/cvar/utils.py
@@ -0,0 +1,6 @@
+# Export all tables from the mdb file
+# for table in $(mdb-tables -1 Izvoz_podatkov.mdb); do
+#     echo "Exporting $table to $table.csv"
+#     mdb-export Izvoz_podatkov.mdb "$table" > "$table.csv"
+# done
+
